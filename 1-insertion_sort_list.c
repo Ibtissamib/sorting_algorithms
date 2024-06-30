906f6847cf64ea778e5e4aeae7db8248f2c84ffc
Ibtissam <ibboutayeb@gmail.com>
2024-06-30 00:42:34 +0000

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *prev = malloc(sizeof(listint_t));
 	listint_t *actual = malloc(sizeof(listint_t));
 
-	if (list || (*list)->next)
+	if (list && (*list)->next)
 	{	node = (*list)->next;
 	head = *list;
 	prev = head->prev;
